9232029ab0826d174c334cc0a2531c27e0671bb1
sushant <sushantlama732@gmail.com>
2024-01-23 19:33:23 +0545

    initialziation

---
+++ apps/docs/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='bg-pink-500 h-screen w-screen overflow-hidden'>
+      App
+    
+     </div>
+  )
+}
+
+export default page
